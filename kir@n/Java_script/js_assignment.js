3e333f3310a9be182c00609d6ae6686cc99aafa9
kiran <kirankiran78904@gmail.com>
2024-08-21 19:40:15 +0530

    Add files via upload

---
+++ kir@n/Java_script/js_assignment.js
@@ -0,0 +1,21 @@
+const taskinput = document.getElementById("taskinput")
+const tasklist = document.getElementById("tasklist")
+
+function addtask(){
+    const tasktext = taskinput.value.trim();
+
+    if(tasktext !==""){
+        const listitem = document.createElement("li");
+        listitem.innerHTML =`<span>${tasktext}</span>
+        <button onclick="deletetask(this)">delete</button>`
+
+        tasklist.appendChild(listitem);
+
+        taskinput.value = ""
+    }
+}
+
+function deletetask(button){
+    const listitem = button.parentElement;
+    tasklist.removeChild(listitem);
+}
